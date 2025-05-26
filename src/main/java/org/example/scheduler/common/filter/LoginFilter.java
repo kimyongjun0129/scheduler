878ba1d774b8702855422852f30f95733ded1d2f
kimyongjun0129 <kimyongjun0129@naver.com>
2025-05-26 14:43:22 +0900

    fix: LoginFilter 화이트 리스트 수정

--- src/main/java/org/example/scheduler/common/filter/LoginFilter.java
+++ src/main/java/org/example/scheduler/common/filter/LoginFilter.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 @Slf4j
 public class LoginFilter implements Filter {
 
-    private static final String[] WHITE_LIST = {"/api/users", "/api/auth/login", "/api/auth/logout"};
+    private static final String[] WHITE_LIST = {"/api/users", "/api/auth/*"};
 
     @Override
     public void doFilter(
